a3039a4ab58435b0fe75f29853e94f7944472d6c
Uriel Alvarez Valdez <azaki14@gmail.com>
2021-04-01 21:32:51 -0600

    port added

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ app.get("/", (req, resp)=> {
   resp.send('Hola mundo');
 })
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
   console.log('server on port 3000')
 })
